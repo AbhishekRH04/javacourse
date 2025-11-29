0a8c66197216129692bda236065df45d89b08bf8
abhishek <abhishekhalgatti270@gmail.com>
2025-11-29 15:20:04 +0530

    Initial commit

---
+++ OneDrive/Documents/NetBeansProjects/javanew/src/javanew/Javanew.java
@@ -0,0 +1,14 @@
+
+package javanew;
+
+
+public class Javanew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
